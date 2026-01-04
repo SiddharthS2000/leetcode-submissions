c1ca7f51b7dabc7705dcf74711ffbbe02bae29d3
SiddharthS2000 <43162987+SiddharthS2000@users.noreply.github.com>
2026-01-04 11:24:57 +0000

    [LeetCode Sync] Runtime - 355 ms (5.93%), Memory - 12.8 MB (6.28%)

---
+++ submissions/0020-valid-parentheses/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isValid(self, s):
+        for x in range(len(s)//2):
+            if s=='': return True
+            s = s.replace('()', '').replace('{}', '').replace('[]', '')
+        return s==''
+                
+	
