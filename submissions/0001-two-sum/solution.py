942cdae3752bc71ed1bc12c5b3a166f84c887715
SiddharthS2000 <43162987+SiddharthS2000@users.noreply.github.com>
2026-08-06 07:35:45 +0000

    [LeetCode Sync] Runtime - 494 ms (33.18%), Memory - 19.8 MB (82.36%)

--- submissions/0001-two-sum/solution.py
+++ submissions/0001-two-sum/solution.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for index1 in range(len(nums)):
-            diff = target - nums[index1]
-            if diff in nums:
-                index2 = nums.index(diff)
-                if index1 != index2:
-                    return index1, index2
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = list()
+        for idx, num in enumerate(nums):
+            diff = target - num
+            if diff in nums and idx != nums.index(diff):
+                return [idx, nums.index(diff)]
+        
