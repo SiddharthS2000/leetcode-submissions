274e0ea0b6c1b370533e694e20b63b155d475d90
SiddharthS2000 <43162987+SiddharthS2000@users.noreply.github.com>
2026-08-19 20:28:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (57.30%)

--- submissions/0027-remove-element/solution.py
+++ submissions/0027-remove-element/solution.py
@@ -1,13 +1,9 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
         for x in nums:
             if x != val:
                 nums[i] = x
                 i += 1
         return i
+
