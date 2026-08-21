89b04dbd73b488cb103b68a3a97e5f6ea7495018
SiddharthS2000 <43162987+SiddharthS2000@users.noreply.github.com>
2026-08-21 10:11:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (19.20%)

--- submissions/0066-plus-one/solution.py
+++ submissions/0066-plus-one/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+
         carry = 1
-        i = len(digits) - 1
-        while i >= 0:
+        for i in range(len(digits) - 1, - 1, -1):
             digits[i] += carry
             carry = digits[i] // 10
             digits[i] = digits[i] % 10
-            if carry == 0:
+            if not carry:
                 return digits
-            i -= 1
+
         else:
             return [1] + digits
-        
+
