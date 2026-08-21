66d7bb4190695783cbfb1a1f6193049d3f12ebb3
SiddharthS2000 <43162987+SiddharthS2000@users.noreply.github.com>
2026-08-21 10:34:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (88.42%)

--- submissions/0027-remove-element/solution.py
+++ submissions/0027-remove-element/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0
-        for x in nums:
-            if x != val:
-                nums[i] = x
-                i += 1
-        return i
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] == val:
+                continue
+            nums[slow] = nums[fast]
+            slow += 1
 
+        return slow
